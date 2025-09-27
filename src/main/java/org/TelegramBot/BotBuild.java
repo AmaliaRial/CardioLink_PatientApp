e56ad3f1005af4087bab0274e31c523098f64b61
AmaliaRial <amalia.rialplaza@usp.ceu.es>
2025-09-27 21:11:58 +0200

    Started with Telegram Bot

---
+++ src/main/java/org/TelegramBot/BotBuild.java
@@ -0,0 +1,21 @@
+package org.TelegramBot;
+
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+
+public class BotBuild {
+    public static void main(String[] args) {
+        String botToken = "8371106425:AAEeBuaIvHEhr_qGl53D3t4tsDEG21Wvqlk";
+        // Using try-with-resources to allow autoclose to run upon finishing
+        try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
+            botsApplication.registerBot(botToken, new CardioLinkBot(botToken));
+            System.out.println("MyAmazingBot successfully started!");
+            // Ensure this prcess wait forever
+            Thread.currentThread().join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
+
