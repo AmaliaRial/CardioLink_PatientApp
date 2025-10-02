c57b3ea6dc6c78f8ec8ec4669fdc256f4ea13dee
loreeenacano <lorena.canodiazmaroto@usp.ceu.es>
2025-10-02 14:07:53 +0200

    patient class

---
+++ src/main/java/Patient.java
@@ -0,0 +1,18 @@
+import common.enums.Sex;
+
+import java.util.Date;
+
+public class Patient {
+    int idPatient;
+    String name;
+    String dni;
+    Date dob;
+    String email;
+    String password;
+    Sex sex;
+    Integer phoneNumber;
+    Integer healthInsuranceNumber;
+    Integer emergencyContact;
+
+
+}
