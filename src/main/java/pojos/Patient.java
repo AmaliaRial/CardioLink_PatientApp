f085cc4238becaf57c89b3b599525d2dcda358f6
carmeencaballero <carmen.caballeroherreros@usp.ceu.es>
2025-10-02 14:13:26 +0200

    modificacionesCarmen

---
+++ src/main/java/pojos/Patient.java
@@ -0,0 +1,18 @@
+import common.enums.Sex;
+
+import java.util.Date;
+
+public class Patient {
+    private int idPatient;
+    private String name;
+    private String dni;
+    private Date dob;
+    private String email;
+    private String password;
+    private Sex sex;
+    private Integer phoneNumber;
+    private Integer healthInsuranceNumber;
+    private Integer emergencyContact;
+
+
+}
