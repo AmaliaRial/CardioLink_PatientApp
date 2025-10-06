9a83a291b05a84b12bbaa4e3077963133edd527b
AnastasiaRicchiuti <anastasia.ricchiuti@gmail.com>
2025-10-06 17:09:21 +0200

    Doctor class addition

---
+++ src/main/java/pojos/Doctor.java
@@ -0,0 +1,69 @@
+package pojos;
+
+import common.enums.Sex;
+import java.util.Date;
+import java.util.Objects;
+
+public class Doctor {
+    private int idDoctor;
+    private String nameDoctor;
+    private String dniDoctor;
+    private Date dobDoctor;
+    private String emailDoctor;
+    // private String passwordDoctor;
+    private Sex sexDoctor;
+
+    public Doctor(int id, String name, String dni, Date dob, String email, Sex sex) {
+        this.idDoctor = id;
+        this.nameDoctor = name;
+        this.dniDoctor = dni;
+        this.dobDoctor = dob;
+        this.emailDoctor = email;
+        this.sexDoctor = sex;
+    }
+
+    public String getNameDoctor() {
+        return nameDoctor;}
+    public void setNameDoctor(String nameDoctor) {
+    this.nameDoctor = nameDoctor;}
+    public String getDniDoctor() {
+        return dniDoctor;}
+    public void setDniDoctor(String dniDoctor) {
+        this.dniDoctor = dniDoctor;}
+    public Date getDobDoctor() {
+        return dobDoctor;}
+    public void setDobDoctor(Date dobDoctor) {
+        this.dobDoctor = dobDoctor;}
+    public String getEmailDoctor() {
+        return emailDoctor;}
+    public void setEmailDoctor(String emailDoctor) {
+        this.emailDoctor = emailDoctor;}
+    public Sex getSexDoctor() {
+        return sexDoctor;}
+    public void setIdDoctor(Sex sexDoctor) {
+        this.sexDoctor = sexDoctor;}
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Doctor doctor = (Doctor) o;
+        return idDoctor == doctor.idDoctor && Objects.equals(nameDoctor, doctor.nameDoctor) && Objects.equals(dniDoctor, doctor.dniDoctor) && Objects.equals(dobDoctor, doctor.dobDoctor) && Objects.equals(emailDoctor, doctor.emailDoctor) && sexDoctor == doctor.sexDoctor;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idDoctor, nameDoctor, dniDoctor, dobDoctor, emailDoctor, sexDoctor);
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "idDoctor=" + idDoctor +
+                ", name='" + nameDoctor + '\'' +
+                ", dni='" + dniDoctor + '\'' +
+                ", dob=" + dobDoctor +
+                ", email='" + emailDoctor + '\'' +
+                ", sex=" + sexDoctor +
+                '}';
+    }
+}
