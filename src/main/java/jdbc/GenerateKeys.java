3fd82d9111415fb915df3b20b45d2893dd1e9538
AmaliaRial <amalia.rialplaza@usp.ceu.es>
2025-10-29 12:07:08 +0100

    deleted unnecessary files from patient APP

--- src/main/java/jdbc/GenerateKeys.java
+++
@@ -1,8 +0,0 @@
-package jdbc;
-
-
-public class GenerateKeys {
-    public static void main(String[] args) throws Exception {
-        Encryption.generateKeys();   // prints both keys to console
-    }
-}
